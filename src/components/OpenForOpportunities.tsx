b9eee79258c0617f774476115a82d20ffd49be5d
Abhishek Diwakar <abhishek88666kumar@gmail.com>
2024-03-16 11:38:22 +0530

    Added contact me in open for opportunities component

--- src/components/OpenForOpportunities.tsx
+++ src/components/OpenForOpportunities.tsx
@@ -1,12 +1,26 @@
+"use client";
+
 import { BadgeCheckIcon } from "lucide-react";
 
 export default function OpenForOpportunities() {
+  const handleContactMeClick = () => {
+    const contactInfoSection = document.querySelector("#connect");
+    if (contactInfoSection) {
+      contactInfoSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    process.env.OPEN_FOR_OPPORTUNITIES === "true" && (
+    process.env.NEXT_PUBLIC_OPEN_FOR_OPPORTUNITIES === "true" && (
       <div className="bg-blue-600 px-3 py-2 text-white">
         <div className="flex gap-2 items-center justify-center">
           <BadgeCheckIcon size={16} />
-          <p className="text-sm">I am currently open for opportunities.</p>
+          <p className="text-sm">
+            I am currently open for opportunities.{" "}
+            <span onClick={handleContactMeClick} className="underline cursor-pointer">
+              Contact me
+            </span>
+          </p>
         </div>
       </div>
     )
--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Social() {
   return (
     <div className="my-6 px-6 sm:p-0">
-      <h4 className="text-2xl font-semibold">Connect</h4>
+      <h4 className="text-2xl font-semibold" id="connect">Connect</h4>
       <p className="text-sm text-slate-400">Connect with me on social media</p>
       <div className="flex flex-col gap-3 mt-2">
         {socialLinks.map((social, index) => (
